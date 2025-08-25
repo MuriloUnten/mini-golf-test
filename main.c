d2953fe71d540f67f3079115a52578fdcad1197f
Murilo Unten <murilo.unten@proton.me>
2025-08-25 20:30:16 -0300

    feat: add power display

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <raylib.h>
 #include <stdio.h>
 
+#define SCREEN_WIDTH 720
+#define SCREEN_HEIGHT 480
+
 typedef struct {
     Vector2 pos;
     Vector2 size;
@@ -12,6 +15,7 @@ typedef struct {
     float   radius;
     Vector2 aim_direction;
     float   aim_theta;
+    float   power;
 } Ball;
 
 int main() {
@@ -21,6 +25,7 @@ int main() {
         .radius = 10.0,
         .aim_direction = { 1.0, 0.0 },
         .aim_theta = 0.0,
+        .power = 0.0,
     };
 
     Entity hole = {
@@ -28,26 +33,32 @@ int main() {
         .size = { 30.0, 30.0 },
     };
 
-    InitWindow(720, 480, "mini golf");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "mini golf");
     while (!WindowShouldClose()) {
-        float dt = GetFPS() / 1000000.0;
+        float dt = 1000.0 / GetFPS();
         
-        // printf("%f\n", aim_theta);
+        float delta_theta = 0.005 * dt;
         if (IsKeyDown(KEY_J)) {
-            ball.aim_theta += 0.2 * dt;
+            ball.aim_theta += delta_theta;
             ball.aim_direction.x = (cos(ball.aim_theta));
             ball.aim_direction.y = (sin(ball.aim_theta));
         }
         if (IsKeyDown(KEY_K)) {
-            ball.aim_theta -= 0.2 * dt;
+            ball.aim_theta -= delta_theta;
             ball.aim_direction.x = (cos(ball.aim_theta));
             ball.aim_direction.y = (sin(ball.aim_theta));
         }
 
-        BeginDrawing();
-        ClearBackground(GREEN);
+        { // Update power
+            ball.power = (SCREEN_HEIGHT - GetMouseY() - 90) / (float)(SCREEN_HEIGHT - 180);
+            if (ball.power > 1) {
+                ball.power = 1.0;
+            }
+            else if (ball.power < 0) {
+                ball.power = 0.0;
+            }
+        }
 
-        DrawFPS(0, 0);
         Vector2 tracer_start_pos = {
             .x = ball.pos.x + (20 * ball.aim_direction.x),
             .y = ball.pos.y + (20 * (-ball.aim_direction.y)),
@@ -58,10 +69,35 @@ int main() {
             .y = tracer_start_pos.y + (60 * (-ball.aim_direction.y)),
         };
 
-        DrawCircle(ball.pos.x, ball.pos.y, ball.radius, RAYWHITE);
-        DrawLineV(tracer_start_pos, tracer_end_pos, RAYWHITE);
+        BeginDrawing();
+        ClearBackground(GREEN);
 
-        DrawCircle(hole.pos.x, hole.pos.y, hole.size.x / 2.0, BLACK);
+        DrawFPS(0, 0);
+
+        { // Draw entities
+            DrawCircle(ball.pos.x, ball.pos.y, ball.radius, RAYWHITE);
+            DrawCircle(hole.pos.x, hole.pos.y, hole.size.x / 2.0, BLACK);
+        }
+
+        { // Draw UI elements
+            DrawLineV(tracer_start_pos, tracer_end_pos, RAYWHITE);
+
+            Rectangle power_background_rect = {
+                .x = 30,
+                .y = 90,
+                . width = 40,
+                .height = 300,
+            };
+            DrawRectangleRec(power_background_rect, BLACK);
+
+            Rectangle power_rect = {
+                .x = 30.0,
+                .y = (SCREEN_HEIGHT - 90 - (int)(300.0 * ball.power)),
+                .width = 40.0,
+                .height = (int)(300.0 * ball.power),
+            };
+            DrawRectangleRec(power_rect, RED);
+        }
 
         EndDrawing();
     }
